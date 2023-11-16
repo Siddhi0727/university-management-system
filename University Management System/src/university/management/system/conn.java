28a4572e48e382b485082f67904c41a6e970c91c
Siddhi <siddhideshmukh007@gmail.com>
2023-11-16 21:33:18 +0530

    Add project ums

---
+++ University Management System/src/university/management/system/conn.java
@@ -0,0 +1,19 @@
+package university.management.system;
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///ums","root","Atharva@100");    
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
